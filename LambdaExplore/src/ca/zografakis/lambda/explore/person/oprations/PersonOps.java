4b366422f05f62545e5d4c6d4996a38dcd3b84cb
nomik <nomik@192.168.1.120>
2017-09-17 10:34:16 -0400

    Initial

---
+++ LambdaExplore/src/ca/zografakis/lambda/explore/person/oprations/PersonOps.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ca.zografakis.lambda.explore.person.oprations;
+
+import ca.zografakis.lambda.explore.person.Person;
+
+/**
+ * @author nomik
+ *
+ */
+public interface PersonOps {
+	
+	String personOperations (Person p);
+	
+}
